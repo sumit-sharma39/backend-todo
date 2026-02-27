fd052188b6b547e9c7b606041e47c34fe2538f5a
Lichtリヒト <sharmarahul1729@gmail.com>
2026-02-27 12:31:11 +0530

    fixed resetpassword case

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const Register = require("./Controller/ResgisterController");
 const GoogleAuth = require ("./Controller/GoogleAuth");
 const GoogleLogin = require ("./Controller/GoogleLogin");
 const ForgotPassword = require("./Controller/forgotpassword");
-const ResetPassword = require("./Controller/ResetPassword");
+const ResetPassword = require("./Controller/resetPassword");
 const requestLogger = require("./middleware/requestLogger");
 
 const auth = require("./middleware/Auth");
