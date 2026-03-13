88303e4b44dbc00da4f41db246962d5cef80ceee
Lichtリヒト <sharmarahul1729@gmail.com>
2026-03-13 16:33:04 +0530

    fixed mailer

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,21 +6,16 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,
-    family: 4, 
+    secure: false, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    tls:{
+        family: 4
+    }
     
 });
 
-transporter.verify((err, success) => {
-  if (err) {
-    console.error("SMTP Error:", err);
-  } else {
-    console.log("SMTP server ready");
-  }
-});
 
 module.exports = transporter;
